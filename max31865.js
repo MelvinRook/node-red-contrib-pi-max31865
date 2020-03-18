65aefceddbf87a9596bd76312ca6528a0232341a
Melvin Rook <melvin@melvinrook.nl>
2020-03-18 15:00:38 +0100

    MAX31865: Adding node functionality

---
+++ max31865.js
@@ -0,0 +1,64 @@
+
+module.exports = function(RED)
+{
+    "use strict";
+    var fs = require('fs');
+
+    // Unlikely if not on a Pi
+    try
+    {
+        var cpuinfo = fs.readFileSync("/proc/cpuinfo").toString();
+
+        if (cpuinfo.indexOf(": BCM") === -1)
+        {
+            throw "Info : "+RED._("rpi-gpio.errors.ignorenode");
+        }
+    }
+    catch(err)
+    {
+        throw "Info : "+RED._("rpi-gpio.errors.ignorenode");
+    }
+
+    // Initialize MAX31865 dependency
+    const MAX31865 = require('max31865');
+
+    // Node-RED main function
+    function PiMax31865Node(n)
+    {
+        RED.nodes.createNode(this, n);
+
+        // Initialize input variables
+        var node = this;
+        this.bus = parseInt(n.bus || 0);             // bus - first digit in /dev/spidev0.0
+        this.device = parseInt(n.dev || 0);          // device - second digit in /dev/spidev0.0
+        this.rtdNominal = parseInt(n.rtd || 1000);   // nominal resistance of sensor
+        this.refResistor = parseInt(n.ref || 4300);  // reference resistance on board
+        this.wires = parseInt(n.wires || 4);         // wires used for sensor (2, 3 or 4)
+
+        // Configure sensor
+        const sensor = new MAX31865(
+            this.bus,
+            this.device,
+            {
+              rtdNominal: this.rtdNominal,
+              refResistor: this.refResistor,
+              wires: this.wires,
+            }
+        );
+
+        // Initialize sensor
+        sensor.init()
+        .then(() =>
+        {
+            // Get temperature from sensor
+            sensor.getTemperature()
+            .then((temperature) =>
+            {
+                // Send payload
+                node.send({payload:temperature, topic:"max31865-"+this.bus+"/"+this.device});
+            });
+        });
+    }
+
+    RED.nodes.registerType("pimax31865",PiMax31865Node);
+}
